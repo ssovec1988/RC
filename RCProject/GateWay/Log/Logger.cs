3e00557746fad4a64f06573cf384c6459d5b1743
s.zorev <zorev2015@mail.ru>
2017-08-02 14:49:48 +0300

    02.08.2017 14 49

---
+++ RCProject/GateWay/Log/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GateWay.Entity;
+using Common;
+
+namespace GateWay.Log
+{
+    public static class Logger
+    {
+        public static void Loging(int np , string error , string kassir)
+        {
+            ModelInitializer init = new ModelInitializer();
+            EFGenericRepository<Loging> model = new EFGenericRepository<Loging>(init.Log);
+
+            model.Add(new Loging { Data = DateTime.Now.ToShortDateString() + DateTime.Now.ToShortTimeString(), ERROR = error, N_KASS = np, KASSIR = kassir });
+        }
+    }
+}
